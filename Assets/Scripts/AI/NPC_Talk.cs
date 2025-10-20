ea481fecf51c331cec3be8dce5f666af6ff4e18f
tonytoes <anthonymiguel2005@gmail.com>
2025-10-20 14:52:22 +0800

    player damage flash

--- Assets/Scripts/AI/NPC_Talk.cs
+++ Assets/Scripts/AI/NPC_Talk.cs
@@ -31,7 +31,8 @@ public class NPC_Talk : MonoBehaviour
     private void Update()
     {
         if(Input.GetButtonDown("Interact"))
-        {
+        {   
+            Debug.Log("Interacting with NPC");
             if (DialogueManager.Instance.isDialogueActive)
             {
                 DialogueManager.Instance.AdvanceDialogue();
